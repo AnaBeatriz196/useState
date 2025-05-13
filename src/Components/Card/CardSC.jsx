8722013bc0b8c7dafe5f2216b7a6bf9a11ffb79c
Ana Beatriz <112020868+AnaBeatriz196@users.noreply.github.com>
2025-05-13 17:54:27 +0000

    alteração 7.0

---
+++ src/Components/Card/CardSC.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Botao = styled.button`
+background: red;
+color: white;
+`;
+
+function App() {
+return <Botao>Clique</Botao>;
+}
---
+++ src/Components/Card/CardTW.jsx
@@ -0,0 +1,4 @@
+<button className="bg-blue-500 text-white px-4 py-2
+rounded-lg">
+Clique
+</button>
